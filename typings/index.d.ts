eb80ea6f3742a221d005ba5132af0b841e1729c9
Pilgrimeru <pilgrim@eru.com>
2024-06-28 15:54:56 +0200

    new dependencies first test

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -1,4 +1,4 @@
-import ytdl from 'ytdl-core';
+import ytdl from '@distube/ytdl-core';
 import { Readable } from 'stream';
 
 declare function download(link: string, options?: ytdl.downloadOptions): Promise<Readable>;
